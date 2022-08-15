3958625abce896140ca5a7ee19f97b216ba69b06
the-darkesttt <belladonna.om@gmail.com>
2022-08-15 23:46:17 +0200

    adding modals, timer, restart btn, mob. version

--- js/newApp.js
+++ js/newApp.js
@@ -1,66 +1,118 @@
-const gameList = document.querySelector(".game-list");
+let stopGame = false;
+let foundedPairs = 0;
+let flippedCardsArr = [];
+
+const gameList = document.querySelector('.game-list');
+
+const gameMenu = document.querySelector('.game-menu');
+const gameMenuTitle = document.querySelector('.modal__title');
+const gameMenuDecs = document.querySelector('.modal__desc');
+const gameStartButton = document.querySelector('#game-start-button');
+
+gameStartButton.addEventListener('click', () => {
+  gameMenu.classList.add('game-menu__hide');
+  countGameTime();
+});
+
+const timerMin = document.querySelector('#timerMin');
+const timerSec = document.querySelector('#timerSec');
+let min = 0;
+let sec = 0;
+
+function countGameTime() {
+  if (!stopGame) {
+    sec++;
+    if (sec === 59) {
+      sec = 0;
+      min++;
+    }
+    timerMin.innerHTML = min;
+    timerSec.innerHTML = sec;
+    setTimeout(countGameTime, 1000);
+  }
+}
+
+const restartButton = document.querySelector('#restart-btn');
+restartButton.addEventListener('click', restartingGame);
+
+function restartingGame() {
+  foundedPairs = 0;
+  sec = 0;
+  min = 0;
+  flippedCardsArr = [];
+  
+  gameList.innerHTML = '';
+
+  shuffleCards(cardSprites);
+  cardsItems = '';
+  createCards();
+  gameList.innerHTML = cardsItems;
+}
+
 
 let cardSprites = [
     {
-        id: "spite-01",
-        src: "img/01.webp",
+        id: 'spite-01',
+        src: 'img/01.webp',
     },
     {
-        id: "spite-02",
-        src: "img/02.webp",
+        id: 'spite-02',
+        src: 'img/02.webp',
     },
     {
-        id: "spite-03",
-        src: "img/03.webp",
+        id: 'spite-03',
+        src: 'img/03.webp',
     },
     {
-        id: "spite-04",
-        src: "img/04.webp",
+        id: 'spite-04',
+        src: 'img/04.webp',
     },
     {
-        id: "spite-05",
-        src: "img/05.webp",
+        id: 'spite-05',
+        src: 'img/05.webp',
     },
     {
-        id: "spite-06",
-        src: "img/06.webp",
+        id: 'spite-06',
+        src: 'img/06.webp',
     },
 ];
 
 function createCard(card) {
     return `
     <li>
-        <div class="flip-container" id="${card.id}">
-            <div class="flipper">
-                <div class="front"></div>
-                <div class="back"><img src="${card.src}"></div>
+        <div class='flip-container' id='${card.id}'>
+            <div class='flipper'>
+                <div class='front'></div>
+                <div class='back'><img src='${card.src}'></div>
             </div>
         </div>
     </li>`;
 }
 
 cardSprites = [...cardSprites, ...cardSprites];
-function shuffle() {
-    cardSprites.sort(() => 0.5 - Math.random());
+function shuffleCards(cards) {
+    cards.sort(() => 0.5 - Math.random());
 }
-shuffle();
+shuffleCards(cardSprites);
 
-let temp = "";
-cardSprites.forEach((sprite) => {
-    temp += createCard(sprite);
-});
+let cardsItems = '';
+function createCards() {
+  cardSprites.forEach((sprite) => {
+    cardsItems += createCard(sprite);
+  });
+}
+createCards();
 
-gameList.innerHTML = temp;
 
-let foundedPairs = 0;
+gameList.innerHTML = cardsItems;
 
-let flippedCardsArr = [];
 
-gameList.addEventListener("click", ({ target }) => {
+
+gameList.addEventListener('click', ({ target }) => {
 
     if (target.closest('.flipper')) {
 
-        flipCard(target)
+        flipCard(target);
 
         if (flippedCardsArr.length === 2) {
 
@@ -71,10 +123,10 @@ gameList.addEventListener("click", ({ target }) => {
 });
 
 function flipCard(target) {
-    const cardFlipContainer = target.closest(".flip-container");
+    const cardFlipContainer = target.closest('.flip-container');
     
     if (flippedCardsArr.length < 2) {
-      cardFlipContainer.classList.add("flip-container-clicked");
+      cardFlipContainer.classList.add('flip-container-clicked');
       flippedCardsArr = [...flippedCardsArr, cardFlipContainer];
     }
 
@@ -82,11 +134,11 @@ function flipCard(target) {
 
 function compareCards() {
 
-  const firstCardID = flippedCardsArr[0].getAttribute("id");
-  const secondCardID = flippedCardsArr[1].getAttribute("id");
+  const firstCardID = flippedCardsArr[0].getAttribute('id');
+  const secondCardID = flippedCardsArr[1].getAttribute('id');
 
   const comparingID = cardSprites.filter((sprite) => {
-    return sprite.id === firstCardID || sprite.id === secondCardID
+    return sprite.id === firstCardID || sprite.id === secondCardID;
   });
 
   if (comparingID.length === 2) {
@@ -102,11 +154,11 @@ function hideMatchingCards() {
   foundedPairs += 2;
 
   setTimeout(() => {
-    flippedCardsArr[0].classList.remove("flip-container-clicked");
-    flippedCardsArr[1].classList.remove("flip-container-clicked");
+    flippedCardsArr[0].classList.remove('flip-container-clicked');
+    flippedCardsArr[1].classList.remove('flip-container-clicked');
 
-    flippedCardsArr[0].classList.add("founded");
-    flippedCardsArr[1].classList.add("founded");
+    flippedCardsArr[0].classList.add('founded');
+    flippedCardsArr[1].classList.add('founded');
 
     flippedCardsArr = []; 
   }, 600);
@@ -118,15 +170,20 @@ function hideMatchingCards() {
 
 function hideUnmatchingCards() {
   setTimeout(() => {
-    flippedCardsArr[0].classList.remove("flip-container-clicked");
-    flippedCardsArr[1].classList.remove("flip-container-clicked");
+    flippedCardsArr[0].classList.remove('flip-container-clicked');
+    flippedCardsArr[1].classList.remove('flip-container-clicked');
 
     flippedCardsArr = []; 
   }, 600);
 }
 
 function showWinMessage() {
+  stopGame = true;
   setTimeout(() => {
-    alert("You won!");
-  }, 600);
+    gameMenuTitle.innerHTML = 'You won!';
+    gameMenuDecs.innerHTML = `Congratulations! You found pairs for all cards 
+      by spending ${min} minutes and ${sec} seconds!`;
+    gameStartButton.remove();
+    gameMenu.classList.remove('game-menu__hide');
+  }, 1000);
 }
